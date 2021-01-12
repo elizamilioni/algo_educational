9a192912b060f167a83e6fe631132dcfd9d9632f
elizamilioni <elizamilioni@yahoo.gr>
2021-01-12 17:26:12 +0200

    mkd almost ready

--- frontend/src/Mkd.js
+++ frontend/src/Mkd.js
@@ -2,9 +2,31 @@ import React, { useState } from 'react';
 
 
 function Mkd() {
-    const [z, setZ] = useState([])
-    const [z1, setZ1] = useState()
+    const [inputX, setInputX] = useState()
+    const [inputY, setInputY] = useState()
+    const [times, setTimes] = useState()
+    const [z, setZ] = useState()
+    const [x, setX] = useState()
+    const [y, setY] = useState()
+    const [correctRes, setCorrectRes] = useState([])
+    const [correctTimes, setCorrectTimes] = useState()
     console.log(z)
+
+    const runAlgo = () => {
+        var x = inputX
+        var y = inputY
+        var z = y
+        var cnt = 0
+        while(z !== 0) {
+            z  = x % y 
+            x = y
+            y = z
+            cnt = cnt + 1
+        }
+        setCorrectTimes(cnt)
+        setCorrectRes([z, x, y])
+        console.log(correctTimes, correctRes, times, z, x, y)
+    }
 
     return (
         <div>
@@ -22,6 +44,22 @@ function Mkd() {
                 <p> Αποτελέσματα x </p>
                 <p> Τέλος Ευκλείδης </p>
             </div>
+            <h4 style={{paddingLeft: '5px'}}> Επιλέξτε τιμές μεταβλητών</h4>
+                <div>
+                    <label style={{paddingLeft: '5px'}}> X:  </label>
+                    <input onChange={() => setInputX()}
+                            style={{borderRadius: '20%', width: '30px', height: '30px'}}/>
+                    <br/> <br/>
+                    <label style={{paddingLeft: '5px'}}> Y:  </label>
+                    <input onChange={() => setInputY()}
+                            style={{borderRadius: '20%', width: '30px', height: '30px'}}/>
+                </div>
+            <h4 style={{paddingLeft: '5px'}}> Αφού τρέξετε τον αλγόριθμο και σημείωσετε όλες τις ενδιάμεσες τιμές 
+                των μεταβλητών απαντήστε στις παρακάτω ερωτήσεις:</h4>
+            <p style={{paddingLeft: '5px'}}> α) Πόσες φορές θα εκτελεστεί η επανάληψη</p>
+            <input onChange={(e) => setTimes(e.target.value)}
+                            style={{borderRadius: '20%', width: '30px', height: '30px', marginLeft: '20px'}}/>
+            <p style={{paddingLeft: '5px'}}> β) Ποιές θα είναι οι τελικές τιμές του αλγορίθμου;</p>
             <table>
                 <tr> 
                     <th> z </th>
@@ -30,10 +68,17 @@ function Mkd() {
                 </tr>
                 <tr>
                     <td> 
-                        <input onChange={(e) => setZ([...z, e.target.value])}/>
+                        <input onChange={(e) => setZ([e.target.value])}/>
+                    </td>
+                    <td> 
+                        <input onChange={(e) => setX([e.target.value])}/>
+                    </td>
+                    <td> 
+                        <input onChange={(e) => setY([e.target.value])}/>
                     </td>
                 </tr>
             </table>
+            <button onClick={runAlgo} style={{marginLeft: '500px'}}> Υποβολή </button>
         </div>
     )
 }
