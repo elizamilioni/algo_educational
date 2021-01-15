673864d51ed53097575c3d3ed168a220d9d8d0fa
elizamilioni <elizamilioni@yahoo.gr>
2021-01-15 12:33:50 +0200

    first level ready except show answer

--- frontend/src/Mkd.js
+++ frontend/src/Mkd.js
@@ -12,6 +12,9 @@ function Mkd() {
     const [x, setX] = useState()
     const [y, setY] = useState()
     const [open, setOpen] = useState(false)
+    const [solution, setSolution] = useState(false)
+    const [messageA, setMessageA] = useState('')
+    const [messageB, setMessageB] = useState('')
 
     const check = (e) => {
         e.preventDefault();
@@ -28,91 +31,118 @@ function Mkd() {
         }
         console.log(correctRes)
         if (times === String(cnt)) {
-            console.log('correct times')
+            setMessageA('a) Σωστό')
         } else {
-            console.log('Not correct times')
+            setMessageA('α) Λάθος')
         }
         if (z === String(zc)) {
             if (x === String(xc)) {
                 if (y === String(yc)) {
-                    console.log('Everything correct')
+                    setMessageB('β) Σωστό')
                 } else {
-                    console.log('Not correct y')
+                    setMessageB('β) Το y είναι λάθος')
                 }
             } else {
-                console.log('Not correct x')
+                setMessageB('β) Το x είναι λάθος')
             }
         } else {
-            console.log('Not corret z')
+            setMessageB('β) Το z είναι λάθος')
         }
         setCorrectRes([])
         setOpen(true)
     }
-    console.log(open)
 
     return (
         <div>
-            <h2 style={{textAlign: 'center'}}> Αλγόριθμος Ευκλείδη</h2>
-            <div style={{paddingLeft: '10px', borderStyle: 'dotted'}}>
-                <p style={{textAlign: 'left'}}> Δεδομένα: x, y </p>
-                <p>z = y </p>
-                <p> Όσο z!=0 επανάλαβε</p>
-                <div style={{paddingLeft: '15px'}}>
-                    <p> z = x mod y </p>
-                    <p> x = y</p>
-                    <p> y = z</p>
-                </div>
-                <p> Τέλος_επανάληψης</p>
-                <p> Αποτελέσματα x </p>
-                <p> Τέλος Ευκλείδης </p>
-            </div>
-            <form onSubmit={check}> 
-                <h4 style={{paddingLeft: '5px'}}> Επιλέξτε τιμές μεταβλητών</h4>
-                    <div>
-                        <label style={{paddingLeft: '5px'}}> X:  </label>
-                        <input onChange={(e) => setInputX(e.target.value)} required
-                                style={{borderRadius: '20%', width: '30px', height: '30px'}}/>
-                        <br/> <br/>
-                        <label style={{paddingLeft: '5px'}}> Y:  </label>
-                        <input onChange={(e) => setInputY(e.target.value)} required
-                                style={{borderRadius: '20%', width: '30px', height: '30px'}}/>
+            <div style={open ? {opacity: '0.3'} : {} }>
+                <h2 style={{textAlign: 'center'}}> Αλγόριθμος Ευκλείδη</h2>
+                <div style={{paddingLeft: '10px', borderStyle: 'dotted'}}>
+                    <p style={{textAlign: 'left'}}> Δεδομένα: x, y </p>
+                    <p>z = y </p>
+                    <p> Όσο z!=0 επανάλαβε</p>
+                    <div style={{paddingLeft: '15px'}}>
+                        <p> z = x mod y </p>
+                        <p> x = y</p>
+                        <p> y = z</p>
                     </div>
-                <h4 style={{paddingLeft: '5px'}}> Αφού τρέξετε τον αλγόριθμο και σημείωσετε όλες τις ενδιάμεσες τιμές 
-                    των μεταβλητών απαντήστε στις παρακάτω ερωτήσεις:</h4>
-                <p style={{paddingLeft: '5px'}}> α) Πόσες φορές θα εκτελεστεί η επανάληψη</p>
-                <input onChange={(e) => setTimes(e.target.value)} required
-                                style={{borderRadius: '20%', width: '30px', height: '30px', marginLeft: '20px'}}/>
-                <p style={{paddingLeft: '5px'}}> β) Ποιές θα είναι οι τελικές τιμές του αλγορίθμου;</p>
-                <table>
-                    <tr> 
-                        <th> z </th>
-                        <th> x </th>
-                        <th> y </th>
-                    </tr>
-                    <tr>
-                        <td> 
-                            <input onChange={(e) => setZ(e.target.value)} required
+                    <p> Τέλος_επανάληψης</p>
+                    <p> Αποτελέσματα x </p>
+                    <p> Τέλος Ευκλείδης </p>
+                </div>
+                <form onSubmit={check}> 
+                    <h4 style={{paddingLeft: '5px'}}> Επιλέξτε τιμές μεταβλητών</h4>
+                        <div>
+                            <label style={{paddingLeft: '5px'}}> X:  </label>
+                            <input onChange={(e) => setInputX(e.target.value)} required
+                                    style={{borderRadius: '20%', width: '30px', height: '30px'}}/>
+                            <br/> <br/>
+                            <label style={{paddingLeft: '5px'}}> Y:  </label>
+                            <input onChange={(e) => setInputY(e.target.value)} required
+                                    style={{borderRadius: '20%', width: '30px', height: '30px'}}/>
+                        </div>
+                    <h4 style={{paddingLeft: '5px'}}> Αφού τρέξετε τον αλγόριθμο και σημείωσετε όλες τις ενδιάμεσες τιμές 
+                        των μεταβλητών απαντήστε στις παρακάτω ερωτήσεις:</h4>
+                    <p style={{paddingLeft: '5px'}}> α) Πόσες φορές θα εκτελεστεί η επανάληψη</p>
+                    <input onChange={(e) => setTimes(e.target.value)} required
+                                    style={{borderRadius: '20%', width: '30px', height: '30px', marginLeft: '20px'}}/>
+                    <p style={{paddingLeft: '5px'}}> β) Ποιές θα είναι οι τελικές τιμές του αλγορίθμου;</p>
+                    <table>
+                        <tr> 
+                            <th> z </th>
+                            <th> x </th>
+                            <th> y </th>
+                        </tr>
+                        <tr>
+                            <td> 
+                                <input onChange={(e) => setZ(e.target.value)} required
+                                        style={{borderRadius: '20%', width: '30px', height: '30px', marginLeft: '5px'}}/>
+                            </td>
+                            <td> 
+                                <input onChange={(e) => setX(e.target.value)} required
                                     style={{borderRadius: '20%', width: '30px', height: '30px', marginLeft: '5px'}}/>
-                        </td>
-                        <td> 
-                            <input onChange={(e) => setX(e.target.value)} required
-                                style={{borderRadius: '20%', width: '30px', height: '30px', marginLeft: '5px'}}/>
-                        </td>
-                        <td> 
-                            <input onChange={(e) => setY(e.target.value)} required
-                                    style={{borderRadius: '20%', width: '30px', height: '30px', marginLeft: '5px'}}/>
-                        </td>
-                    </tr>
-                </table>
-                <button style={{marginLeft: '5px', backgroundColor:'#282c34', 
-                                borderRadius: '50%', borderWidth: '1px', width: '100px',
-                                height: '50px'}}> 
-                            Υποβολή 
-                </button>
-            </form>
-            <Popup open={open} onClose={() => setOpen(false)}>
-                <div style={{borderStyle: 'dotted', backgroundColor: 'transparent'}}>
-                    <p>Message </p>
+                            </td>
+                            <td> 
+                                <input onChange={(e) => setY(e.target.value)} required
+                                        style={{borderRadius: '20%', width: '30px', height: '30px', marginLeft: '5px'}}/>
+                            </td>
+                        </tr>
+                    </table>
+                    <button style={{marginLeft: '5px', backgroundColor:'#282c34', 
+                                    borderRadius: '50%', borderWidth: '1px', width: '100px',
+                                    height: '50px'}}> 
+                                Υποβολή 
+                    </button>
+                </form>
+            </div>
+            <Popup open={open} onClose={() => {setOpen(false); setMessageA(''); setMessageB('')}}>
+                <div style={{borderStyle: 'solid', backgroundColor: 'gray', height: '120px', width: '200px'}}>
+                    <p>{messageA} </p>
+                    <p>{messageB} </p>
+                    <button onClick={() => setSolution(true)}
+                            style={{marginLeft: '100px', backgroundColor:'#A3FF33', 
+                            borderRadius: '20%', borderWidth: '1px', width: '100px',
+                            height: '30px'}}>
+                        Δες τη λύση 
+                    </button>
+                </div>
+            </Popup>
+            <Popup open={solution} onClose={() => setSolution(false)}>
+                <div style={{borderStyle: 'solid', backgroundColor: 'gray', height: '120px', width: '200px'}}>
+                    <table>
+                        <tr> 
+                            <th> z </th>
+                            <th> x </th>
+                            <th> y </th>
+                        </tr>
+                        {correctRes.map((row, i) => {
+                            console.log('ei', row) (
+                            // <tr>
+                            //     <td> {row[0]} </td>
+                            //     <td> {row[1]} </td>
+                            //     <td> {row[2]} </td>
+                            // </tr>
+                        )})}
+                    </table>                     
                 </div>
             </Popup>
         </div>
